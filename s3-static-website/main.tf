c9b4fd45c8c9d333aba1f9ea084f9526eda3f585
Elroy Antao <elroyantao@gmail.com>
2021-03-23 21:34:22 +0000

    minor fix for cloudfront alias

--- s3-static-website/main.tf
+++ s3-static-website/main.tf
@@ -53,7 +53,7 @@ resource "aws_cloudfront_distribution" "cloudfront" {
 
   enabled         = true
   is_ipv6_enabled = true
-  aliases         = [vardomain, "www.${var.domain}"]
+  aliases         = [var.domain, "www.${var.domain}"]
 
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD"]
