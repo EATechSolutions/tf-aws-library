89ca4bb34c69968fa3c8311bfd579396c6c18416
Elroy Antao <elroyantao@gmail.com>
2021-03-24 00:13:16 +0000

    fix route53

--- s3-static-website/main.tf
+++ s3-static-website/main.tf
@@ -97,8 +97,8 @@ resource "aws_route53_record" "www" {
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution._.domain_name
-    zone_id                = aws_cloudfront_distribution._.hosted_zone_id
+    name                   = aws_cloudfront_distribution._[0].domain_name
+    zone_id                = aws_cloudfront_distribution._[0].hosted_zone_id
     evaluate_target_health = false
   }
 }
